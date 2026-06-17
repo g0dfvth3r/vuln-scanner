bb6c7de30784a3c174f9567f8e5ed6bfb418b04f
Marcos Furtado <marcos.furtado01@gmail.com>
2026-06-17 22:37:41 +0200

    add security headers check

--- scanner.py
+++ scanner.py
@@ -11,3 +11,16 @@ headers = r.headers
 
 for key, value in headers.items():
     print(key, ":", value)
+
+headers_to_check = [
+    "Content-Security-Policy",
+    "Strict-Transport-Security",
+    "X-Frame-Options",
+    "X-Content-Type-Options"
+]
+print('\nSecurity Headers:')
+for header in headers_to_check:
+    if header in headers:
+        print(f'[FOUND]     {header}')
+    else:
+        print(f'[NOT FOUND] {header}')
