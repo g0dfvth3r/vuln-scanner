d657ad23269578ddc06b4b8df8b64a031ea2c06e
Marcos Furtado <marcos.furtado01@gmail.com>
2026-06-17 21:54:01 +0200

    print headers in readable format

--- scanner.py
+++ scanner.py
@@ -4,5 +4,10 @@ import requests
 url = sys.argv[1]
 if not url.startswith("http"):
     url = "https://" + url
+
 r = requests.get(url)
-print(r.status_code, r.headers)
+print(f"Status code: {r.status_code}\n")
+headers = r.headers
+
+for key, value in headers.items():
+    print(key, ":", value)
