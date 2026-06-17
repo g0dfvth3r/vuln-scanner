ffde06e53d8ed47fd1c06ce262be388f1d9f7126
Marcos Furtado <marcos.furtado01@gmail.com>
2026-06-17 23:55:37 +0200

    add cookie flag checks

--- scanner.py
+++ scanner.py
@@ -18,9 +18,31 @@ headers_to_check = [
     "X-Frame-Options",
     "X-Content-Type-Options"
 ]
+
 print('\nSecurity Headers:')
 for header in headers_to_check:
     if header in headers:
         print(f'[FOUND]     {header}')
     else:
         print(f'[NOT FOUND] {header}')
+
+cookie_header_to_check = [
+    " secure",
+    " httponly",
+    " samesite"
+]
+
+print('\nCookies:')
+for raw_cookie in r.raw.headers.getlist("Set-Cookie"):
+    parts = raw_cookie.split(";")
+    cookie_name = parts[0].split("=")[0].strip()
+    cookie_lower = raw_cookie.lower()
+    
+    print(f"\n  {cookie_name}")
+    
+    for flag in ["secure", "httponly", "samesite"]:
+        if flag in cookie_lower:
+            print(f"    [FOUND]     {flag}")
+        else:
+            print(f"    [NOT FOUND] {flag}")
+    
